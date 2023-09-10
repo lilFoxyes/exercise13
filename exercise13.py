828f6d6749719246433ebd637492b77a1892c7d3
Foxyes <108577021+lilFoxyes@users.noreply.github.com>
2023-09-10 15:05:28 -0300

    Add files via upload

---
+++ exercise13.py
@@ -0,0 +1,3 @@
+s = int(input("Digite o seu salário e eu irei mostrar como ele irá ficar com 15% de aumento: "))
+a = s*1.15
+print("O seu antigo salário era: {} reais, agora com o aumento será: {} reais".format(s, a))
